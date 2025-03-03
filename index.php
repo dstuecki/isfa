aa5e82779510dd29fecc2a9455fc7f870d989170
dstuecki <dstuecki@gmail.com>
2025-03-03 09:37:26 +0100

    first upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Gaming Blog</title>
+    </head>
+    <body>
+        <div id="area01">
+            <div id="header">
+
+            </div>
+            <div id="slider">
+
+            </div>
+            <div id="menue">
+
+            </div>
+            <div id="submenue">
+
+            </div>
+            <div id="content">
+
+            </div>
+            <div id="buttom">
+                
+            </div>
+        </div>
+    </body>
+</html>
